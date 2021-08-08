845cd7f0dda643a3214277a03a84e28b70b52b15
tanvo1999 <tanminhvo12340@gmail.com>
2021-08-09 00:37:16 +0700

    update

--- TieuThuyetVN/src/screen/Login.js
+++ TieuThuyetVN/src/screen/Login.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Button, Input, ListItem, Text } from 'react-native-elements';
 import { StyleSheet, Dimensions, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import LinearGradient from 'react-native-linear-gradient';
+import TouchableScale from 'react-native-touchable-scale';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
@@ -12,7 +15,7 @@ const FormLogin = () => {
             <View style={{widthL: width, alignItems: 'center', marginVertical: 20}}>
                 <Text style={{fontWeight: '700'}} h3>Đăng nhập</Text>
             </View>
-            <View style={{marginHorizontal: 20}}>
+            <View style={{marginHorizontal: 20, justifyContent: 'center', alignItems: 'center'}}>
                 <Input 
                     label="Email"
                     placeholder="abc@gmail.com"
@@ -34,34 +37,67 @@ const FormLogin = () => {
                         color='black'
                     />} 
                 />
-                <Button 
-                    title="Đăng nhập"
-                    type="outline" 
-                    titleStyle={styles.textButton}
-                    style={[styles.button, {backgroundColor: '#4b0082'}]}
-                />
-                {/* <Button 
-                    title="Đăng nhập"
-                    type="outline" 
-                    titleStyle={styles.textButton}
-                    style={[styles.button, {backgroundColor: '#4b0082', marginTop: 40, paddingVertical:5}]}
-                /> */}
-                <Button 
-                    title="Facebook"
-                    type="outline" 
-                    iconPosition="left"
-                    icon={<Icon name="facebook-square" size={22} color={'#FFF'} />}
-                    titleStyle={styles.textButton}
-                    style={[styles.button, {backgroundColor: '#1877f2'}]}
-                 />
-                <Button 
-                    title="Google"
-                    type="outline" 
-                    iconPosition="left"
-                    icon={<Icon name="google" size={22} color={'#FFF'} />}
-                    titleStyle={styles.textButton}
-                    style={[styles.button, {backgroundColor: '#ea4335'}]}
-                />
+
+                <ListItem 
+                    onPress={() => console.log(1)}
+                    Component={TouchableScale}
+                    friction={90}
+                    tension={100}
+                    activeScale={0.85}
+                    linearGradientProps={{
+                        colors: ['#bc7fe8', '#4b0082'],
+                        start: { x: 1, y: 0 },
+                        end: { x: 0.2, y: 0 },
+                    }}
+                    ViewComponent={LinearGradient}
+                    containerStyle={styles.button}
+                >
+                    <ListItem.Content style={{alignItems: 'center'}}>
+                        <ListItem.Title numberOfLines={1} style={styles.textButton}>
+                            Đăng nhập
+                        </ListItem.Title>
+                    </ListItem.Content>
+                </ListItem>
+                <ListItem 
+                    onPress={() => console.log(1)}
+                    Component={TouchableScale}
+                    friction={90}
+                    tension={100}
+                    activeScale={0.85}
+                    linearGradientProps={{
+                        colors: ['#ff7261', '#f2260d'],
+                        start: { x: 1, y: 0 },
+                        end: { x: 0.2, y: 0 },
+                    }}
+                    ViewComponent={LinearGradient}
+                    containerStyle={styles.button}
+                >
+                    <ListItem.Content style={{alignItems: 'center'}}>
+                        <ListItem.Title numberOfLines={1} style={styles.textButton}>
+                        <Icon name="google" size={22} color={'#FFF'} /> {'  '}Google
+                        </ListItem.Title>
+                    </ListItem.Content>
+                </ListItem>
+                <ListItem 
+                    onPress={() => console.log(1)}
+                    Component={TouchableScale}
+                    friction={90}
+                    tension={100}
+                    activeScale={0.85}
+                    linearGradientProps={{
+                        colors: ['#61a0f2', '#1877f2'],
+                        start: { x: 1, y: 0 },
+                        end: { x: 0.2, y: 0 },
+                    }}
+                    ViewComponent={LinearGradient}
+                    containerStyle={styles.button}
+                >
+                    <ListItem.Content style={{alignItems: 'center'}}>
+                        <ListItem.Title numberOfLines={1} style={styles.textButton}>
+                            <Icon name="facebook-square" size={22} color={'#FFF'} /> {'  '}Facebook
+                        </ListItem.Title>
+                    </ListItem.Content>
+                </ListItem>
             </View>
         </View>
     )
@@ -70,13 +106,14 @@ const FormLogin = () => {
 const styles = StyleSheet.create({
     textButton: {
         fontSize: 20, 
-        fontWeight: 'bold',
-        color: 'white',
-        marginLeft: 5,
-        paddingVertical:5
+        fontWeight: 'bold', 
+        color: '#fff'
     },
     button: {
-        marginTop: 20,
+        marginTop: 10, 
+        height: 70, 
+        width: '90%', 
+        borderRadius: 15,
     }
 });
 
